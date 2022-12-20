de60e922ee25dced8250f269858725e328c08bac
Teamjbworks <116152922+Teamjbworks@users.noreply.github.com>
2022-12-20 21:59:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function btn(){
+  window.open("https://g.co/kgs/bWKZZb");
+}
+function btn3() {
+  let text = "\n Can you give some rating stars to our products!?";
+  if (confirm(text) == true) {
+    alert("Successfully Submitted !");
+    window.open("https://g.page/r/CVURTrTgpJH3ECA/review")
+  } else {
+    window.open("thx.html")
+  }
+  document.getElementById("demo").innerHTML = text;
+}
+function btn2(){
+  window.open("https://instagram.com/jb_wall_paintings?igshid=YmMyMTA2M2Y=");
+}
