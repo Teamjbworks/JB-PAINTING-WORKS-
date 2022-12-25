ad0961a077d3a123d2e8c53e39b3c86330c4923f
Teamjbworks <116152922+Teamjbworks@users.noreply.github.com>
2022-12-25 20:27:57 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 function btn(){
   window.open("https://g.co/kgs/bWKZZb");
 }
-function btn3() {
-    alert("Successfully Submitted !");
-    window.open("thx.html");
-  document.getElementById("demo").innerHTML = text;
-}
 function btn2(){
   window.open("https://instagram.com/jb_wall_paintings?igshid=YmMyMTA2M2Y=");
+}
+function validateForm() {
+  let x = document.forms["sheetdb-form"].value;
+  if (x == "") {
+    alert("Form must be filled out");
+    return false;
+  }else{
+    window.location.href="thx.html";
+  }
 }
